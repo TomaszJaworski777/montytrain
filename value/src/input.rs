5d25202801750e72c4f1c34de772c220e4aea2a9
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-21 12:09:51 +0100

    -StageA

--- value/src/input.rs
+++ value/src/input.rs
@@ -7,7 +7,7 @@ impl inputs::SparseInputType for ThreatInputs {
     type RequiredDataType = ChessBoard;
 
     fn num_inputs(&self) -> usize {
-        BASE_INPUTS + STATE_INPUTS
+        STATE_INPUTS
     }
 
     fn max_active(&self) -> usize {
@@ -49,23 +49,11 @@ impl inputs::SparseInputType for ThreatInputs {
             let attackers = all_attackers & board.occupancy_for_side(board.side().flipped()) & !attack_pin_mask;
             let defenders = all_attackers & board.occupancy_for_side(board.side()) & !defender_pin_mask;
 
-            let (attacker, defender) = attacker_defender(board, attackers, defenders);
-
             let piece_index = 64 * (u8::from(piece) - u8::from(Piece::PAWN)) as usize;
             let base = [384, 0][usize::from(color == board.side())] + piece_index + (usize::from(square) ^ horizontal_mirror);
-            let mut feat = base;
 
-            if attacker != Piece::NONE {
-                feat += 768 * (usize::from(attacker) + 1)
-            }
+            let feat = 768 * calculate_state(board, piece, attackers, defenders) + base;
 
-            if defender != Piece::NONE {
-                feat += 768 * 7 * (usize::from(defender) + 1)
-            }
-
-            f(feat, feat);
-
-            let feat = BASE_INPUTS + 768 * calculate_state(piece, attacker, defender, attackers, defenders) + base;
             f(feat, feat)
         });
     }
@@ -79,38 +67,12 @@ impl inputs::SparseInputType for ThreatInputs {
     }
 }
 
-const BASE_INPUTS: usize = 768 * 7 * 7;
 const STATE_INPUTS: usize = 768 * 6;
 
-fn attacker_defender(board: &chess::ChessBoard, attackers: Bitboard, defenders: Bitboard) -> (Piece, Piece) {
-    let attacker = lowest_value_piece(board, attackers);
-    let defender = lowest_value_piece(board, defenders);
-    (attacker, defender)
-}
+fn calculate_state(board: &chess::ChessBoard, victim: Piece, attackers: Bitboard, defenders: Bitboard) -> usize {
+    let lowest_attacker = lowest_value_piece(board, attackers);
+    let lowest_defender = lowest_value_piece(board, defenders);
 
-fn lowest_value_piece(board: &chess::ChessBoard, mask: Bitboard) -> Piece {
-    if mask.is_empty() {
-        return Piece::NONE;
-    }
-
-    if (mask & board.piece_mask(Piece::PAWN)).is_not_empty() {
-        Piece::PAWN
-    } else if (mask & board.piece_mask(Piece::KNIGHT)).is_not_empty() {
-        Piece::KNIGHT
-    } else if (mask & board.piece_mask(Piece::BISHOP)).is_not_empty() {
-        Piece::BISHOP
-    } else if (mask & board.piece_mask(Piece::ROOK)).is_not_empty() {
-        Piece::ROOK
-    } else if (mask & board.piece_mask(Piece::QUEEN)).is_not_empty() {
-        Piece::QUEEN
-    } else if (mask & board.piece_mask(Piece::KING)).is_not_empty() {
-        Piece::KING
-    } else {
-        Piece::NONE
-    }
-}
-
-fn calculate_state(victim: Piece, lowest_attacker: Piece, lowest_defender: Piece, attackers: Bitboard, defenders: Bitboard) -> usize {
     let atk_cnt = attackers.pop_count();
     let def_cnt = defenders.pop_count();
 
@@ -159,5 +121,27 @@ fn calculate_state(victim: Piece, lowest_attacker: Piece, lowest_defender: Piece
         return 2;
     } else {
         return 3
+    }
+}
+
+fn lowest_value_piece(board: &chess::ChessBoard, mask: Bitboard) -> Piece {
+    if mask.is_empty() {
+        return Piece::NONE;
+    }
+
+    if (mask & board.piece_mask(Piece::PAWN)).is_not_empty() {
+        Piece::PAWN
+    } else if (mask & board.piece_mask(Piece::KNIGHT)).is_not_empty() {
+        Piece::KNIGHT
+    } else if (mask & board.piece_mask(Piece::BISHOP)).is_not_empty() {
+        Piece::BISHOP
+    } else if (mask & board.piece_mask(Piece::ROOK)).is_not_empty() {
+        Piece::ROOK
+    } else if (mask & board.piece_mask(Piece::QUEEN)).is_not_empty() {
+        Piece::QUEEN
+    } else if (mask & board.piece_mask(Piece::KING)).is_not_empty() {
+        Piece::KING
+    } else {
+        Piece::NONE
     }
 }
--- value/src/main.rs
+++ value/src/main.rs
@@ -26,7 +26,7 @@ fn main() {
     let mut trainer = make_trainer::<ThreatInputs>(HIDDEN_SIZE);
 
     let schedule = TrainingSchedule {
-        net_id: "StageB".to_string(),
+        net_id: "StageB-A".to_string(),
         eval_scale: 400.0,
         steps: TrainingSteps {
             batch_size: 16384,
