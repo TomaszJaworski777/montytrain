2e4f77afc1c8299b75fa7b354897a1d90ba25034
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2025-11-22 22:12:39 +0100

    Stage D1

--- value/src/input.rs
+++ value/src/input.rs
@@ -1,7 +1,8 @@
 use bullet::game::{formats::bulletformat::ChessBoard, inputs};
-use chess::{Bitboard, Piece, Rays, Side};
+use chess::{Attacks, Bitboard, Piece, Rays, Side, Square};
 
 const STATE_INPUTS: usize = 768 * 6 * 2 * 2;
+const KING_ALIGN: usize = 128 * 3 * 2;
 
 #[derive(Clone, Copy, Default)]
 pub struct ThreatInputs;
@@ -9,11 +10,11 @@ impl inputs::SparseInputType for ThreatInputs {
     type RequiredDataType = ChessBoard;
 
     fn num_inputs(&self) -> usize {
-        STATE_INPUTS
+        STATE_INPUTS + KING_ALIGN
     }
 
     fn max_active(&self) -> usize {
-        32
+        58
     }
 
     fn map_features<F: FnMut(usize, usize)>(&self, board: &Self::RequiredDataType, mut f: F) {
@@ -37,10 +38,11 @@ impl inputs::SparseInputType for ThreatInputs {
             0
         };
 
-        //let rings = [Attacks::get_king_attacks(board.king_square(Side::WHITE)), Attacks::get_king_attacks(board.king_square(Side::BLACK))];
+        let rings = [Attacks::get_king_attacks(board.king_square(Side::WHITE)), Attacks::get_king_attacks(board.king_square(Side::BLACK))];
 
         let occ = board.occupancy();
-        let mut side_offset = 0;
+        let mut base_side_offset = 0;
+        let mut align_side_offset = 0;
         for piece_color in [Side::WHITE, Side::BLACK] {
             let defender_pin_mask = diag_stm | ortho_stm;
             let attack_pin_mask = diag_nstm | ortho_nstm;
@@ -48,12 +50,15 @@ impl inputs::SparseInputType for ThreatInputs {
             let occ_stm = board.occupancy_for_side(piece_color);
             let occ_nstm = board.occupancy_for_side(piece_color.flipped());
 
+            let enemy_king = board.king_square(piece_color.flipped());
+
             for piece_idx in u8::from(Piece::PAWN)..=u8::from(Piece::KING) {
                 let piece = Piece::from(piece_idx);
-                let feat_idx = side_offset + 64 * (u8::from(piece) - u8::from(Piece::PAWN)) as usize;
+                let feat_idx = base_side_offset + 64 * (u8::from(piece) - u8::from(Piece::PAWN)) as usize;
 
                 board.piece_mask_for_side(piece, piece_color).map(|square| {
-                    let attack_pin_mask = attack_pin_mask & !Rays::get_ray(square, board.king_square(piece_color.flipped()));
+                    let king_attack_ray = Rays::get_ray(square, enemy_king);
+                    let attack_pin_mask = attack_pin_mask & !king_attack_ray;
 
                     let all_attackers = board.all_attackers_to_square(occ, square);
                     let attackers = all_attackers & occ_nstm & !attack_pin_mask;
@@ -73,11 +78,44 @@ impl inputs::SparseInputType for ThreatInputs {
 
                     feat += base;
 
-                    f(feat, feat)
+                    f(feat, feat);
+
+                    let ring = rings[usize::from(piece_color.flipped())];
+                    let is_ring_xrayed = if piece == Piece::BISHOP {
+                        (BISHOP_ATTACKS[usize::from(square)] & ring).is_not_empty()
+                    } else if piece == Piece::ROOK {
+                        (ROOK_ATTACKS[usize::from(square)] & ring).is_not_empty()
+                    } else if piece == Piece::QUEEN {
+                        ((BISHOP_ATTACKS[usize::from(square)] | ROOK_ATTACKS[usize::from(square)]) & ring).is_not_empty()
+                    } else {
+                        false
+                    };
+                    
+                    let king_bb = Bitboard::from_square(enemy_king);
+                    let is_king_xrayed = if piece == Piece::BISHOP {
+                        (BISHOP_ATTACKS[usize::from(square)] & king_bb).is_not_empty()
+                    } else if piece == Piece::ROOK {
+                        (ROOK_ATTACKS[usize::from(square)] & king_bb).is_not_empty()
+                    } else if piece == Piece::QUEEN {
+                        ((BISHOP_ATTACKS[usize::from(square)] | ROOK_ATTACKS[usize::from(square)]) & king_bb).is_not_empty()
+                    } else {
+                        false
+                    };
+
+                    if piece_idx >= u8::from(Piece::BISHOP) && piece_idx <= u8::from(Piece::QUEEN) && (is_king_xrayed || is_ring_xrayed) {
+                        let mut feat = STATE_INPUTS + align_side_offset + 64 * (u8::from(piece) - u8::from(Piece::BISHOP)) as usize;
+                        
+                        if is_king_xrayed {
+                            feat += 384
+                        }
+
+                        f(feat, feat);
+                    }
                 });
             }
 
-            side_offset += 384;
+            base_side_offset += 384;
+            align_side_offset += 192;
             (diag_stm, ortho_stm, diag_nstm, ortho_nstm) = (diag_nstm, ortho_nstm, diag_stm, ortho_stm);
         }
     }
@@ -171,4 +209,98 @@ fn lowest_value_piece(board: &chess::ChessBoard, mask: Bitboard) -> Piece {
     } else {
         Piece::NONE
     }
+}
+
+const BISHOP_ATTACKS: [Bitboard; 64] = {
+    let mut result = [Bitboard::EMPTY; 64];
+    let mut square_index = 0u8;
+    while square_index < 64 {
+        result[square_index as usize] = mask_bishop_attacks(Square::from_value(square_index));
+        square_index += 1;
+    }
+    result
+};
+
+const ROOK_ATTACKS: [Bitboard; 64] = {
+    let mut result = [Bitboard::EMPTY; 64];
+    let mut square_index = 0u8;
+    while square_index < 64 {
+        result[square_index as usize] = mask_rook_attacks(Square::from_value(square_index));
+        square_index += 1;
+    }
+    result
+};
+
+const fn mask_bishop_attacks(square: Square) -> Bitboard {
+    let mut result: u64 = 0;
+    let bishop_position = (square.get_rank() as i32, square.file() as i32);
+
+    let mut rank = bishop_position.0 + 1;
+    let mut file = bishop_position.1 + 1;
+    while rank <= 7 && file <= 7 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank += 1;
+        file += 1;
+    }
+
+    rank = bishop_position.0 - 1;
+    file = bishop_position.1 + 1;
+    while rank >= 0 && file <= 7 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank -= 1;
+        file += 1;
+    }
+
+    rank = bishop_position.0 - 1;
+    file = bishop_position.1 - 1;
+    while rank >= 0 && file >= 0 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank -= 1;
+        file -= 1;
+    }
+
+    rank = bishop_position.0 + 1;
+    file = bishop_position.1 - 1;
+    while rank <= 7 && file >= 0 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank += 1;
+        file -= 1;
+    }
+
+    Bitboard::from_value(result)
+}
+
+const fn mask_rook_attacks(square: Square) -> Bitboard {
+    let mut result: u64 = 0;
+    let rook_position = (square.get_rank() as i32, square.file() as i32);
+
+    let mut rank = rook_position.0 + 1;
+    let mut file = rook_position.1;
+    while rank <= 7 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank += 1;
+    }
+
+    rank = rook_position.0 - 1;
+    file = rook_position.1;
+    while rank >= 0 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        rank -= 1;
+    }
+
+    rank = rook_position.0;
+    file = rook_position.1 + 1;
+    while file <= 7 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        file += 1;
+    }
+
+    rank = rook_position.0;
+    file = rook_position.1 - 1;
+    while file >= 0 {
+        result |= 1 << Square::from_coords(rank as u8, file as u8).get_value();
+        file -= 1;
+    }
+
+    Bitboard::from_value(result)
 }
