6e1dc6c9418e3d9b9cee033693f83fc4451a5e02
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-11 01:11:07 +0100

    Update main.rs

--- value/src/main.rs
+++ value/src/main.rs
@@ -64,12 +64,13 @@ fn main() {
         let fen = pos.as_fen();
         let mut castling = Castling::default();
         castling.parse(pos, &fen);
-        let filter = pos.stm() == 0 && result > 0.9 && qsearch(pos, &castling, -30000, 30000, 0) < -300;
+        let filter = (pos.stm() == 0 && result > 0.9 && qsearch(pos, &castling, -30000, 30000, 0) < -300) 
+            || (pos.stm() == 1 && result < 0.1 && -qsearch(pos, &castling, -30000, 30000, 0) < -300);
 
         if filter {
             println!("passed {}", pos.as_fen())
         } else {
-            println!("not passed {}", pos.as_fen())
+            println!("not passed {}, result: {}", pos.as_fen())
         }
 
         filter
