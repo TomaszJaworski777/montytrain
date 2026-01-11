132546674cf76d892f804589efc1e3bb1c874123
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-11 22:52:15 +0100

    Update main.rs

--- value/src/main.rs
+++ value/src/main.rs
@@ -75,11 +75,9 @@ fn main() {
             return false;
         }
 
-        return true;
+        // let pos = &Position::from_raw(pos.bbs(), pos.stm() == 1, pos.enp_sq(), 0, pos.halfm(), pos.fullm());
 
-        let pos = &Position::from_raw(pos.bbs(), pos.stm() == 1, pos.enp_sq(), 0, pos.halfm(), pos.fullm());
-
-        let mut castling = Castling::default();
+        // let mut castling = Castling::default();
         let qs_score = -500; //qsearch(pos, &castling, -30000, 30000, 0);
 
         let white_sac = pos.stm() == 0 && result > 0.9 && qs_score < -300 && qs_score > -2000;
