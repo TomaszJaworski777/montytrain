d3f86d104dbe446e0db914febe924470e42e96cd
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-11 04:13:52 +0100

    Update main.rs

--- value/src/main.rs
+++ value/src/main.rs
@@ -213,7 +213,7 @@ fn calculate_material(pos: &Position) -> i32 {
 }
 
 fn mv_is_check(mv: Move, pos: &Position, castling: &Castling) -> bool {
-    return true;
+    return false;
     let mut pos_clone = pos.clone();
     pos_clone.make(mv, castling);
     pos_clone.in_check()
