687d67e6f1f6d2ec1099a9fe9272229f133e3047
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-11 03:24:18 +0100

    Update main.rs

--- value/src/main.rs
+++ value/src/main.rs
@@ -69,7 +69,7 @@ fn main() {
         let mut castling = Castling::default();
         castling.parse(pos, &fen);
         let filter = (pos.stm() == 0 && result > 0.9 && qsearch(pos, &castling, -30000, 30000, 0) < -300) 
-            || (pos.stm() == 1 && result < 0.1 && -qsearch(pos, &castling, -30000, 30000, 0) < -300);
+            || (pos.stm() == 1 && result < 0.1 && qsearch(pos, &castling, -30000, 30000, 0) < -300);
 
         if filter && pos.get_pc(1 << mv.src()) != Piece::PAWN {
             println!("passed with result {result}: {}", fen)
