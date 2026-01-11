f610646a3b0af0e8b0f02d07d28bea4410916e2d
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-11 18:04:12 +0100

    Update main.rs

--- value/src/main.rs
+++ value/src/main.rs
@@ -27,7 +27,7 @@ pub const QB: i16 = 1024;
 fn main() {
     let mut trainer = make_trainer::<ThreatInputs>(HIDDEN_SIZE);
 
-    let size = 2 * 1024 * 1024 / std::mem::size_of::<ChessBoard>() / 2;
+    let size = 256 * 1024 * 1024 / std::mem::size_of::<ChessBoard>() / 2;
 
     let schedule = TrainingSchedule {
         net_id: format!("MontyThreatsFT2-{size}"),
