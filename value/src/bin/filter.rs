0202839b065bc0de56d213e19d8e5da1ef61ca75
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-01-12 16:05:17 +0100

    Update filter.rs

--- value/src/bin/filter.rs
+++ value/src/bin/filter.rs
@@ -76,15 +76,13 @@ fn main() -> std::io::Result<()> {
                         }
 
                         let filter = |pos: &Position, result: f32, material: i32| -> bool {
-                            let draw = result == 0.5 && material.abs() > 300;
-
                             let white_sac = pos.stm() == 0 && result > 0.9 && material < -300 && material > -2000;
                             let black_sac = pos.stm() == 1 && result < 0.1 && material < -300 && material > -2000;
 
                             let n_white_sac = pos.stm() == 0 && result < 0.1 && material > 300;
                             let n_black_sac = pos.stm() == 1 && result > 0.9 && material > 300;
 
-                            draw || white_sac || black_sac || n_white_sac || n_black_sac
+                            white_sac || black_sac || n_white_sac || n_black_sac
                         };
 
                         if !filter(pos, result, calculate_material(pos)) {
