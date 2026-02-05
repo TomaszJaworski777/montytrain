b065c46173be985abfa28f2f1c9e5f8a4ce3d47a
TomaszJaworski777 <71073337+TomaszJaworski777@users.noreply.github.com>
2026-02-05 12:51:31 +0100

    adjust filter

--- policy/src/bin/filter.rs
+++ policy/src/bin/filter.rs
@@ -176,11 +176,11 @@ fn is_aggressive_win(pos: &Position, castling: &Castling, data: &SearchData, gam
     }
 
     let filter = |pos: &Position, result: f32, material: i32| -> bool {
-        let white_sac = pos.stm() == 0 && result > 0.9 && material < -300 && material > -2000;
-        let black_sac = pos.stm() == 1 && result < 0.1 && material < -300 && material > -2000;
+        let white_sac = pos.stm() == 1 && result > 0.9 && material < -300 && material > -2000;
+        let black_sac = pos.stm() == 0 && result < 0.1 && material < -300 && material > -2000;
 
-        let n_white_sac = pos.stm() == 0 && result < 0.1 && material > 300;
-        let n_black_sac = pos.stm() == 1 && result > 0.9 && material > 300;
+        let n_white_sac = pos.stm() == 1 && result < 0.1 && material > 300 && material < 2000;
+        let n_black_sac = pos.stm() == 0 && result > 0.9 && material > 300 && material < 2000;
 
         white_sac || black_sac || n_white_sac || n_black_sac
     };
